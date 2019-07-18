9bbeb82dbf855f80a26b8ad474735836b9c554e3
Suprie <suprie.donk.punya.email@gmail.com>
2019-07-18 12:00:39 +0700

    Initial Commit

---
+++ Google CastTests/Google_CastTests.swift
@@ -0,0 +1,34 @@
+//
+//  Google_CastTests.swift
+//  Google CastTests
+//
+//  Created by Supriyanto P on 18/07/19.
+//  Copyright © 2019 Supriyanto P. All rights reserved.
+//
+
+import XCTest
+@testable import Google_Cast
+
+class Google_CastTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
